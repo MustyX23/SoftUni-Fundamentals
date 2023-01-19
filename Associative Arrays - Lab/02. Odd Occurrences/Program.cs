5068f2331fbffb5d603a72593d7a281fc7bde7c0
MustyX23 <mustyx900@gmail.com>
2023-01-19 23:57:39 +0200

    -Odd Occurrences-

--- Associative Arrays - Lab/02. Odd Occurrences/Program.cs
+++ Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -9,6 +9,29 @@ namespace _02._Odd_Occurrences
     {
         static void Main(string[] args)
         {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (!map.ContainsKey(wordInLowerCase))
+                {
+                    map.Add(wordInLowerCase, 1);
+                }
+                else
+                {
+                    map[wordInLowerCase]++;
+                }
+            }
+            foreach (var item in map)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
             
         }
     }   
