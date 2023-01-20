c966051d7a8f2901a23d7032a2a1b1f6b268a131
MustyX23 <mustyx900@gmail.com>
2023-01-20 14:23:03 +0200

    Associative Arrays + Exercises

---
+++ Exercise - Associative Arrays/01. Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char,int>map=new Dictionary<char,int>();
+
+            string text = Console.ReadLine();
+
+            foreach (var ch in text)
+            {
+                if (!map.ContainsKey(ch))
+                {
+                    map.Add(ch, 1);
+                }
+                else
+                {
+                    map[ch]++;
+                }
+            }
+
+            foreach (KeyValuePair <char, int> ch in map.Where(x => x.Key != ' '))
+            {
+                Console.WriteLine($"{ch.Key} -> {ch.Value}");
+            }
+        }
+    }
+}
---
+++ Exercise - Associative Arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                string ore = input;
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!map.ContainsKey(ore))
+                {
+                    map.Add (ore, quantity);
+                }
+                else
+                {
+                    map[ore] += quantity;
+                }
+               
+            }
+
+            foreach (var item in map)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
