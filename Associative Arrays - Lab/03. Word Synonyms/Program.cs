14c265bc66532f051db8f9d660dafdde4cb9714e
MustyX23 <mustyx900@gmail.com>
2023-01-20 11:25:14 +0200

    -Word Synonyms-

--- Associative Arrays - Lab/03. Word Synonyms/Program.cs
+++ Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -8,8 +8,27 @@ namespace _03._Word_Synonyms
     {
         static void Main(string[] args)
         {
-            
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
 
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonyms.ContainsKey(word))
+                {
+                    synonyms.Add(word, new List<string>());
+                }
+                synonyms[word].Add(synonym);                
+                
+            }
+
+            foreach (var item in synonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
         }
     }
 }
