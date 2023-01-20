5de01b45aa46d65c34fa6c6168c6225bfb5f4a55
MustyX23 <mustyx900@gmail.com>
2023-01-20 15:20:09 +0200

    -Orders-
    Create a program that keeps the information about products and their prices. Each product has a name, a price and a quantity. If the product doesn't exist yet, add it with its starting quantity.
    If you receive a product, which already exists, increase its quantity by the input quantity and if its price is different, replace the price as well.
    You will receive products' names, prices and quantities on new lines. Until you receive the command "buy", keep adding items. When you do receive the command "buy", print the items with their names and the total price of all the products with that name.
    Input
    •	Until you receive "buy", the products will be coming in the format: "{name} {price} {quantity}".
    •	The product data is always delimited by a single space.
    Output
    •	Print information about each product in the following format:
    "{productName} -> {totalPrice}"
    •	Format the average grade to the 2nd digit after the decimal separator.

---
+++ Exercise - Associative Arrays/03. Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> map = new Dictionary<string, List<double>>();
+
+            string input;
+
+            while ((input = Console.ReadLine())!= "buy")
+            {
+                string[] products = input.Split(' ');
+
+                string name = products[0];
+                double price = double.Parse(products[1]);
+                double quantity = double.Parse(products[2]);
+
+                if (!map.ContainsKey(name))
+                {
+                    map.Add(name, new List<double>() {price, quantity});
+                }
+                else
+                {
+                    map[name][0] = price;
+                    map[name][1] += quantity;
+                }
+            }
+
+            foreach (var item in map)
+            {
+                Console.WriteLine($"{item.Key} -> {(item.Value[0] * item.Value[1]):f2}");
+            }
+        }
+    }
+}
