e57bf2750324ca54ad8fdbb7844268584e262492
MustyX23 <mustyx900@gmail.com>
2023-01-22 18:31:29 +0200

    -Reverse Strings-

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split();
+            StringBuilder result = new StringBuilder();
+
+            foreach (var item in strings)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    result.Append(item);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
