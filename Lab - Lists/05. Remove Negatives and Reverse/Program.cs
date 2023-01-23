04343745e1032cb21561c49b6f3de712bc94ae57
MustyX23 <mustyx900@gmail.com>
2023-01-23 22:27:36 +0200

    -05. Remove Negatives and Reverse-

--- Lab - Lists/05. Remove Negatives and Reverse/Program.cs
+++ Lab - Lists/05. Remove Negatives and Reverse/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._Remove_Negatives_and_Reverse
 {
@@ -6,7 +8,16 @@ namespace _05._Remove_Negatives_and_Reverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse)
+                .Where(x => x >= 0).ToList();
+
+            list.Reverse();
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            Console.WriteLine(String.Join(' ', list));
         }
     }
 }
