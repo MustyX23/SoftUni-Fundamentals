1e9e544a55728f8480257e255570619ef19410c5
MustyX23 <mustyx900@gmail.com>
2023-01-24 20:23:02 +0200

    06. List Manipulation Basics

--- Lab - Lists/06. List Manipulation Basics/Program.cs
+++ Lab - Lists/06. List Manipulation Basics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06._List_Manipulation_Basics
 {
@@ -6,7 +8,40 @@ namespace _06._List_Manipulation_Basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] tokens = input.Split(' ');
+                string command = tokens[0];
+
+                if (command == "Add")
+                {
+                    int addingNumber = int.Parse(tokens[1]);
+                    list.Add(addingNumber);
+                }
+                else if (command == "Remove")
+                {
+                    int removingNumber = int.Parse(tokens[1]);
+                    list.Remove(removingNumber);
+                }
+                else if (command == "RemoveAt")
+                {
+                    int removingNumberAt = int.Parse(tokens[1]);
+                    list.RemoveAt(removingNumberAt);
+                }
+                else if (command == "Insert")
+                {
+                    int numberInsert = int.Parse(tokens[1]);
+                    int indexInsert = int.Parse(tokens[2]);
+
+                    list.Insert(indexInsert, numberInsert);
+                }
+            }
+
+            Console.WriteLine(String.Join(' ', list));
         }
     }
 }
