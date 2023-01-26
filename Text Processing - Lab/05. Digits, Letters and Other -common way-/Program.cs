54e6d0a9e3d9d2141c3c9e74a707787b65db3996
MustyX23 <mustyx900@gmail.com>
2023-01-26 21:04:38 +0200

    Update -Common way-

---
+++ Text Processing - Lab/05. Digits, Letters and Other -common way-/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05._Digits__Letters_and_Other__common_way_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string other = string.Empty;
+
+            foreach (var item in text)
+            {
+                if (Char.IsDigit(item))
+                {
+                    digits += item;
+                }
+                else if (Char.IsLetter(item))
+                {
+                    letters += item;
+                }
+                else 
+                {
+                    other += item;
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(other);
+
+        }
+    }
+}
