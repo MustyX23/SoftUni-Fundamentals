279c7979e5b8afd58c3fccf2bc43c33afeda34ef
MustyX23 <mustyx900@gmail.com>
2023-01-29 20:30:10 +0200

    02. Division

---
+++ Exercise Basic Syntax, Conditional Statements and Loops/02. Division/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int[] divs = { 10, 7, 6, 3, 2 };
+
+            for (int i = 0; i < divs.Length; i++)
+            {
+                int divisibleNumber = divs[i];
+
+                if (number % divisibleNumber == 0)
+                {
+                    Console.WriteLine($"The number is divisible by {divisibleNumber}");
+                    return;
+                }
+            }
+                
+            Console.WriteLine("Not divisible");
+        }
+    }
+}
