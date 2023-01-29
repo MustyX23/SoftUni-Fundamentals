6438d81ea25d00d47da5966ce372864e998e9ae0
MustyX23 <mustyx900@gmail.com>
2023-01-29 21:13:53 +0200

    04. Print and sum

---
+++ Exercise Basic Syntax, Conditional Statements and Loops/04. Print and sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int lastNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = firstNumber; i <= lastNumber; i++)
+            {
+                
+                sum += i;
+                Console.Write($"{i} ");                
+            }
+
+            Console.WriteLine($"\nSum: {sum}");
+        }
+    }
+}
