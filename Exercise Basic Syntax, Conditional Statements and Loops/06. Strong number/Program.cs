49344a1349d3e61eb5cbcd234175dad4e9ede3f5
MustyX23 <mustyx900@gmail.com>
2023-01-29 21:38:26 +0200

    06. Strong number

---
+++ Exercise Basic Syntax, Conditional Statements and Loops/06. Strong number/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<int> digits = GetDigitsList(number);
+
+            int sumFactoriels = 0;
+
+            foreach (int digit in digits)
+            {
+                sumFactoriels += GetFactoriel(digit);
+            }
+
+            if (sumFactoriels == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else 
+            {
+                Console.WriteLine("no");
+            } 
+
+        }
+
+        static int GetFactoriel(int n)
+        {
+            if (n <= 1) return 1;
+            return n * GetFactoriel(n - 1);
+        }
+
+        static List<int> GetDigitsList(int number)
+        {
+            List<int> digits = new List<int>();
+            while (number != 0)
+            {
+                digits.Add(number % 10);
+                number /= 10;
+            }
+            return digits;
+        }
+
+
+    }
+}
