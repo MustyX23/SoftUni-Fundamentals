c159b040e1161fbbd6035e90b2fb73a89d0af31e
MustyX23 <mustyx900@gmail.com>
2023-01-29 22:58:24 +0200

    -Left to be Done-

---
+++ Exercise Basic Syntax, Conditional Statements and Loops/09. Padawan Equipment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Left to be done...
+        }
+    }
+}
