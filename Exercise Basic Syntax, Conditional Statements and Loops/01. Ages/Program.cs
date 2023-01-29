2b12bb59932c0befcc5fc329a11f0a3c294b1b9f
MustyX23 <mustyx900@gmail.com>
2023-01-29 20:26:26 +0200

    01. Ages

---
+++ Exercise Basic Syntax, Conditional Statements and Loops/01. Ages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            if (age <= 2 && age >= 0)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (age <= 13 && age >= 3)
+            {
+                Console.WriteLine("child");
+            }
+            else if (age <= 19 && age >= 14)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (age <= 65 && age >= 20)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (age >= 65)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
