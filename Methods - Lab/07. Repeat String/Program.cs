b4726cebc0231232fe98ecc65515f69b025256c6
MustyX23 <mustyx900@gmail.com>
2023-02-01 20:45:08 +0200

    07. Repeat String

---
+++ Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string context = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine(StringRepeater(context, count));
+        }
+        static string StringRepeater(string context, int count)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                stringBuilder.Append(context);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
