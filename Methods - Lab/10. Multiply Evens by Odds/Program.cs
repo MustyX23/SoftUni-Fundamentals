1d4b40d6565bbba57e6f9e3c8239c9d2182cfe13
MustyX23 <mustyx900@gmail.com>
2023-02-01 20:47:33 +0200

    Not ready

---
+++ Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
