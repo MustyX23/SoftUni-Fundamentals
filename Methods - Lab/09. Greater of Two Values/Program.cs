5d06e2d2a098efc4fc640ec0fadeb66a30eb0c22
MustyX23 <mustyx900@gmail.com>
2023-02-01 20:46:48 +0200

    09. Greater of Two Values

---
+++ Methods - Lab/09. Greater of Two Values/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                double round = Math.Round(arr[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{arr[i]} => {round}");
+            }
+        }
+    }
+}
