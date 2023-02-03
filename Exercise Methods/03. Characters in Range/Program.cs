d2d9b8fad0e34b81b42fd0e8e681fda5c8d0a3fb
MustyX23 <mustyx900@gmail.com>
2023-02-03 21:00:01 +0200

    03. Characters in Range

---
+++ Exercise Methods/03. Characters in Range/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            CharactersInRange(firstChar, secondChar);
+        }
+
+        static void CharactersInRange(char firstChar, char secondChar)
+        {
+            if (secondChar < firstChar)
+            {
+                char temp = firstChar;
+                firstChar = secondChar;
+                secondChar = temp;
+            }
+
+            for (int i = (int)firstChar + 1; i < (int)secondChar; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+            
+        }
+    }
+}
