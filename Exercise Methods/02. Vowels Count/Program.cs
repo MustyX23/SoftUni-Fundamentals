3fb2c5b8ca2697eea1ea4583926f527bd8ac76cd
MustyX23 <mustyx900@gmail.com>
2023-02-03 20:59:48 +0200

    02. Vowels Count

---
+++ Exercise Methods/02. Vowels Count/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine(GetVowelsCount(text));
+        }
+
+        static int GetVowelsCount(string text)
+        {
+            int count = 0;
+            foreach (var c in text)
+            {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
