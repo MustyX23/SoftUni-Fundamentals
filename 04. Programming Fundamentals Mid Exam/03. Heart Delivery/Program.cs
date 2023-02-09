e6abcf0ed1fbb230d3b5b232e8dd88009e373723
MustyX23 <mustyx900@gmail.com>
2023-02-09 20:59:22 +0200

    03. Heart Delivery

---
+++ 04. Programming Fundamentals Mid Exam/03. Heart Delivery/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Heart_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> houses = Console.ReadLine().Split('@').Select(int.Parse).ToList();
+
+            string input;
+            while ((input = Console.ReadLine())!= "Love!")
+            {
+                string[] commands = input.Split(' ');
+                string action = commands[0];
+                int length = int.Parse(commands[1]);
+
+                if (action == "Jump")
+                {
+                    //TODO...
+                }
+            }
+        }
+    }
+}
