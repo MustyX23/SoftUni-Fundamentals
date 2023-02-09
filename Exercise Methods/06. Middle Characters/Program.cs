bb1063e0c89f88d55f2170622466771a00d435c5
MustyX23 <mustyx900@gmail.com>
2023-02-09 21:48:21 +0200

    06. Middle Characters

---
+++ Exercise Methods/06. Middle Characters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine(); //someText -> 012 34 567 - Length = 8
+            MiddleChar(input);
+        }
+        static void MiddleChar(string input)
+        {
+            int index = input.Length / 2;
+            
+
+            if (input.Length % 2 == 0)
+            {
+                char firstIndex = input[index - 1];
+                char secondIndex = input[index];
+                string result = firstIndex.ToString() + secondIndex.ToString();
+
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(input[index]);
+            }
+            
+        }
+    }
+}
