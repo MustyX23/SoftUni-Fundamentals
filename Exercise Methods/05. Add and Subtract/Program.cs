27d4c0dd217ffd2359efecbe6336c42485f6e1d9
MustyX23 <mustyx900@gmail.com>
2023-02-09 21:48:09 +0200

    05. Add and Subtract

---
+++ Exercise Methods/05. Add and Subtract/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int sum = Sum(firstNum, secondNum);
+            int result = (sum - thirdNum);
+
+            Console.WriteLine(result);
+        }
+        static int Sum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        static int Substract(int firstNum, int secondNum)
+        {
+            return firstNum - secondNum;
+        }
+    }
+}
