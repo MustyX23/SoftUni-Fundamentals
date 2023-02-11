4eb013853ee8926ef2fa4320a1a63bcc3fe5eb7d
MustyX23 <mustyx900@gmail.com>
2023-02-11 19:23:12 +0200

    08. Factorial Division

---
+++ Exercise Methods/08. Factorial Division/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FactorielsDevicion(firstNum, secondNum));                       
+        }
+        static double FactorielsDevicion(int firstNum, int secondNum)
+        {
+            double firstFactoriel = 1;
+            double secondFactoriel = 1;
+
+            for (int i = firstNum; i > 0; i--)
+            {
+                firstFactoriel *= i;
+            }
+            for (int i = secondNum; i > 0; i--)
+            {
+                secondFactoriel *= i;
+            }
+
+            double result = firstFactoriel - secondFactoriel;
+            return result;
+        }
+    }
+}
