0d5615ea24315aebc1bd4eff9fcef067127fd90b
MustyX23 <mustyx900@gmail.com>
2023-02-11 19:20:05 +0200

    Create Program.cs

---
+++ Exercise Methods/07. NxN Matrix/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            NxN(n);
+        }
+
+        static void NxN(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write($"{number} ");
+                for (int j = 0; j < number - 1; j++)
+                {
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
