cddce0ba5d30e6f2510db6ab923c9acf6e0bd821
MustyX23 <mustyx900@gmail.com>
2023-02-11 19:23:30 +0200

    09. Palindrome Integers

---
+++ Exercise Methods/09. Palindrome Integers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 10;
+            Console.WriteLine(isValid(a, b));
+            
+        }
+        static bool isValid(int a, int b)
+        {
+            return a == b;
+        }
+    }
+}
