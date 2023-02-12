c90a4df9be11403339b89e1b1763bd450031206d
MustyX23 <mustyx900@gmail.com>
2023-02-12 10:29:06 +0200

    01. Computer Store

---
+++ 01. Programming Fundamentals Mid Exam Retake/01. Computer Store/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double sumPartsPrices = 0;
+            double taxPrices = 0;
+
+            while (input != "regular" && input != "special")
+            {
+                double partsPrices = double.Parse(input);
+                if (partsPrices > 0)
+                {
+                    sumPartsPrices += partsPrices;
+                    taxPrices = sumPartsPrices * 0.2;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid price!");
+                }
+                input = Console.ReadLine();
+            }
+            double totalPrice = sumPartsPrices + taxPrices;
+            double discount = totalPrice * 0.10;
+            if (input == "special")
+            {
+                totalPrice -= discount;
+            }
+            if (totalPrice <= 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {sumPartsPrices:f2}$");
+            Console.WriteLine($"Taxes: {taxPrices:f2}$");
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Total price: {totalPrice:f2}$");
+        }
+    }
+}
