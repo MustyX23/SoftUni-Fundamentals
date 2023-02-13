7e047353c559b606f10481adb4a70aa13c15a677
MustyX23 <mustyx900@gmail.com>
2023-02-13 23:11:28 +0200

    01. Counter-Strike

---
+++ 03. Programming Fundamentals Mid Exam/01. Counter-Strike/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01._Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            int wonBattles = 0;
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (energy >= distance)
+                {
+                    energy -= distance;
+                    wonBattles++;
+                    if (wonBattles % 3 == 0)
+                    {
+                        energy += wonBattles;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"Won battles: {wonBattles}. Energy left: {energy}");
+
+        }
+    }
+}
