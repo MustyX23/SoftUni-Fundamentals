fedc0ea2e913b9b0c2c220cd216eedd18adbfd7e
MustyX23 <mustyx900@gmail.com>
2023-02-14 23:57:44 +0200

    03. Moving Target

---
+++ 03. Programming Fundamentals Mid Exam/0.0 Demos/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Parse the input
+        List<int> list = new List<int>();
+        string[] input = Console.ReadLine().Split(' ');
+        foreach (string s in input)
+        {
+            int value;
+            if (int.TryParse(s, out value))
+            {
+                list.Add(value);
+            }
+        }
+
+        // Execute the commands
+        string command = Console.ReadLine();
+        while (command != "End")
+        {
+            string[] parts = command.Split(' ');
+            if (parts[0] == "Strike")
+            {
+                int index, radius;
+                if (int.TryParse(parts[1], out index) && int.TryParse(parts[2], out radius))
+                {
+                    // Validate the index and radius
+                    if (index >= 0 && index < list.Count)
+                    {
+                        if (radius <= 0 || radius > list.Count / 2)
+                        {
+                            Console.WriteLine("Invalid radius!");
+                        }
+                        else
+                        {
+                            // Remove items within the radius
+                            int start = Math.Max(0, index - radius);
+                            int end = Math.Min(list.Count - 1, index + radius);
+                            while (end >= start)
+                            {
+                                list.RemoveAt(end);
+                                end--;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Strike missed!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid command!");
+            }
+            command = Console.ReadLine();
+        }
+
+        // Print the final list
+        Console.WriteLine(string.Join("|", list));
+    }
+}
+
---
+++ 03. Programming Fundamentals Mid Exam/03. Moving Target/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Moving_Target
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string input;
+
+            while ((input= Console.ReadLine())!= "End")
+            {
+                string[] command = input.Split(' ');
+                string action = command[0];
+
+                if (action == "Shoot")
+                {
+                    //Shoot {index} {power}
+                    int index = int.Parse(command[1]);
+                    int power = int.Parse(command[2]);
+
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets[index] -= power;
+
+                        if (targets[index] <= 0)
+                        {
+                            targets.RemoveAt(index);
+                        }
+                    }
+                }
+                else if (action == "Add")
+                {
+                    //Add {index} {value}
+                    int index = int.Parse(command[1]);
+                    int value = int.Parse(command[2]);
+
+                    if (index >= 0 && index < targets.Count)
+                    {
+                        targets.Insert(index, value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement!");
+                    }
+                }
+                else if (action == "Strike")
+                {
+                    //Strike {index} {radius}
+                    int index = int.Parse(command[1]);
+                    int value = int.Parse(command[2]);
+
+                    int radius = 1 + (value * 2);
+
+                    int radiusStart = index - value;
+                    int radiusEnd = index + value;
+
+                    if (radiusStart >= 0 && radiusEnd < targets.Count)
+                    {
+                        targets.RemoveRange(radiusStart, radius);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Strike missed!");
+                    }
+
+                }
+            }
+
+            Console.WriteLine(String.Join('|',targets));
+        }
+    }
+}
