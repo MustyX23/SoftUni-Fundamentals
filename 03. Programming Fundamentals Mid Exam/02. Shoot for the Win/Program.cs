ad52063a1711dcf228b9f3bf08bdab54a7c6bb15
MustyX23 <mustyx900@gmail.com>
2023-02-14 18:20:02 +0200

    02. Shoot for the Win

--- 03. Programming Fundamentals Mid Exam/02. Shoot for the Win/Program.cs
+++ 03. Programming Fundamentals Mid Exam/02. Shoot for the Win/Program.cs
@@ -9,24 +9,38 @@ namespace _02._Shoot_for_the_Win
         static void Main(string[] args)
         {
             List<int>targets = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-
+            //24 50 36 70
             string input;
+            int count = 0;
 
             while ((input = Console.ReadLine()) != "End")
             {
                 int index = int.Parse(input);
-
-                targets.RemoveAt(index);
-                targets.Insert(index, -1);
-
-                for (int i = 1; i < targets.Count - 1; i++)
+                
+                if (index >= 0 && index < targets.Count)
                 {
-                    if (targets[i] > targets[index])
-                    {
-                        targets[i] -= targets[index];
+                    for (int i = 0; i < targets.Count; i++)
+                    {                       
+                        if (i!= index && targets[i] != -1)
+                        {
+                            if (targets[i] > targets[index])
+                            {
+                                targets[i] -= targets[index];
+                            }
+                            else
+                            {
+                                targets[i] += targets[index];
+                            }
+                        }                       
                     }
-                }
+
+                    targets[index] = -1;
+                    count++;
+                }                                                 
             }
+
+            Console.WriteLine($"Shot targets: {count} -> {string.Join(' ', targets)}");
+
         }
     }
 }
