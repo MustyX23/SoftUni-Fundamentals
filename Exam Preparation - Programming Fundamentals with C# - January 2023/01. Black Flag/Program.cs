9429c767dddf2db8245d9cf5755eebcbceea5797
MustyX23 <mustyx900@gmail.com>
2023-02-18 00:07:49 +0200

    01. Black Flag

--- Exam Preparation - Programming Fundamentals with C# - January 2023/01. Black Flag/Program.cs
+++ Exam Preparation - Programming Fundamentals with C# - January 2023/01. Black Flag/Program.cs
@@ -6,7 +6,38 @@ namespace _01._Black_Flag
     {
         static void Main(string[] args)
         {
-            
+            int days = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double currentPlunder = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                currentPlunder += dailyPlunder;
+
+                if (i % 3 == 0)
+                {
+                    double additionalPlunder = dailyPlunder * 0.5;
+                    currentPlunder += additionalPlunder;
+                }
+                if (i % 5 == 0)
+                {
+                    double lostPlunder = currentPlunder * 0.3;
+                    currentPlunder -= lostPlunder;
+                }
+            }
+
+            if (currentPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {currentPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                double percentageLeft = currentPlunder / expectedPlunder * 100;
+                Console.WriteLine($"Collected only {percentageLeft:f2}% of the plunder.");
+            }
+
         }
     }
 }
