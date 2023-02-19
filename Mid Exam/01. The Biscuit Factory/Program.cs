5ea9a226295f71dcc4b166b149b3452ad458d4a3
MustyX23 <mustyx900@gmail.com>
2023-02-19 22:53:24 +0200

    01. The Biscuit Factory

---
+++ Mid Exam/01. The Biscuit Factory/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class BisquitsFactory
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0;
+
+        int bisquitsForAdayWorker = int.Parse(Console.ReadLine());
+        int workers = int.Parse(Console.ReadLine());
+        int factoryCapacity = int.Parse(Console.ReadLine());
+
+        for (int day = 1; day <= 30; day++)
+        {
+            int biscuitsForADay = bisquitsForAdayWorker * workers;
+            if (day % 3 == 0)
+            {
+                biscuitsForADay = (int)(biscuitsForADay * 0.75);
+            }
+            sum += biscuitsForADay;
+        }
+
+        Console.WriteLine($"You have produced {sum} biscuits for the past month.");
+
+        int differencec = Math.Abs(factoryCapacity - sum);
+        double percentage = ((double)differencec / factoryCapacity) * 100;
+
+        if (factoryCapacity > sum)
+        {
+            Console.WriteLine($"You produce {percentage:F2} percent less biscuits.");
+        }
+        else
+        {
+            Console.WriteLine($"You produce {percentage:F2} percent more biscuits.");
+        }
+    }
+}
