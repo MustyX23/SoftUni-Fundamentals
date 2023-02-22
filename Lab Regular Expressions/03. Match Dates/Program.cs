8f2f402449d818c2213e4ce08a444d1cd21a8821
MustyX23 <mustyx900@gmail.com>
2023-02-22 21:52:47 +0200

    03. Match Dates

---
+++ Lab Regular Expressions/03. Match Dates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})(\/|-|.)(?<month>\w{3})\1(?<year>\d{4})\b";
+
+            string input = Console.ReadLine();
+
+
+            MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+            foreach (Match item in matchCollection)
+            {
+                var day = item.Groups["day"];
+                var month = item.Groups["month"];
+                var year = item.Groups["year"];
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+        }
+    }
+}
