c8c9591ca811b21624939f288cb80deb583f2364
MustyX23 <mustyx900@gmail.com>
2023-02-22 21:52:31 +0200

    02. Match Phone Number

---
+++ Lab Regular Expressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace _02._Match_Phone_Number
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+(359)( |-)2\2\d{3}\2\d{4}\b";
+
+            string phoneNumbers = Console.ReadLine();
+
+            var matchcollection = Regex.Matches(phoneNumbers, pattern);
+
+            List<string> listForPrint = new List<string>();
+
+            foreach (Match item in matchcollection)
+            {
+                listForPrint.Add(item.Value);
+            }
+
+            Console.WriteLine(string.Join(", ", listForPrint));
+
+        }
+    }
+}
