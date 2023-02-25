a58f1ade469d1e1e2a3785435690fbfd61540d04
MustyX23 <mustyx900@gmail.com>
2023-02-25 21:40:23 +0200

    01. Furniture

--- Regular Expressions - Exercise/01. Furniture/Program.cs
+++ Regular Expressions - Exercise/01. Furniture/Program.cs
@@ -1,51 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace _01._Furniture
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-
-            double price = 0;
-            int quantity = 0;
+            string pattern = @">>(?<name>[A-Za-z]+)<<(?<price>\d+\.?\d*)!(?<quantity>\d+)";
+            List<string> nameList = new List<string>();
             double totalPrice = 0;
+            bool enter = false;
 
-            bool isMatched = false;
 
-            string pattern = @">>(?<furniture>[A-Za-z]+)<<(?<price>\d*\.?\d*)!(?<quantity>\d*)";
-            List<string> nameList= new List<string>();
-            
-            while (input != "Purchase")
+            while (true)
             {
-                //">>{furniture name}<<{price}!{quantity}"
 
+                if (input == "Purchase")
+                {
+                    break;
+                }
                 Regex regex = new Regex(pattern);
                 Match match = regex.Match(input);
-
                 if (match.Success)
                 {
-                    nameList.Add(match.Groups["furniture"].Value);
-                    price = double.Parse(match.Groups["price"].Value);
-                    quantity = int.Parse(match.Groups["quantity"].Value);
-
-                    totalPrice += price * quantity;
-                    isMatched = true;
+                    nameList.Add(match.Groups["name"].Value);
+                    totalPrice += double.Parse(match.Groups["price"].Value) * double.Parse(match.Groups["quantity"].Value);
+                    enter = true;
                 }
-
                 input = Console.ReadLine();
-            }
 
-            if (isMatched == true)
+            }
+            if (enter == true)
             {
                 Console.WriteLine("Bought furniture:");
-                Console.WriteLine(String.Join(Environment.NewLine, nameList));
-                Console.WriteLine($"Total money spend: {totalPrice}");
+                Console.WriteLine(string.Join(Environment.NewLine, nameList));
+                Console.WriteLine($"Total money spend: {totalPrice:f2}");
             }
+            else
+            {
+                Console.WriteLine("Bought furniture:");
+                Console.WriteLine($"Total money spend: {totalPrice:f2}");
+            }
+
 
         }
     }
