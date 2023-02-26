fc74a6d52ff1d5a358a2f5e8921a0c5219b4ffd3
MustyX23 <mustyx900@gmail.com>
2023-02-26 23:08:29 +0200

    02. Race

---
+++ Regular Expressions - Exercise/02. Race/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(", ").ToList();
+
+            string input;
+
+            while ((input = Console.ReadLine())!= "end of the race")
+            {
+                string namePattern = @"(?<name>[A-Za-z]+)";
+                MatchCollection nameCollection = Regex.Matches(input, namePattern);               
+
+                string numbersPattern = @"(?<numbers>\d)";
+                MatchCollection numberCollection = Regex.Matches(input, numbersPattern);
+            }
+        }
+    }
+}
