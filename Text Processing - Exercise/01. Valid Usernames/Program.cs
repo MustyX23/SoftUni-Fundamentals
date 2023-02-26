3b0715aaab98bc30c8f2a5d40e69231fe8951263
MustyX23 <mustyx900@gmail.com>
2023-02-26 23:08:42 +0200

    01. Valid Usernames

---
+++ Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = Console.ReadLine().Split(", ");
+
+            List<string> validUsernames = new List<string>();
+
+            for (int i = 0; i < userNames.Length; i++)
+            {
+                string user = userNames[i];
+                if (user.Length >= 3 && user.Length <= 16)
+                {
+                    bool validUsername = ValidUserNames(user);
+                    if (validUsername == true)
+                    {
+                        validUsernames.Add(user);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, validUsernames));
+        }
+
+        static bool ValidUserNames(string user)
+        {
+            foreach (char c in user)
+            {
+                if (char.IsLetterOrDigit(c) || c == '-' || c == '_')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+        
+    }
+}
