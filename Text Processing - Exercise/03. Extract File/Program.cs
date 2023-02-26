7c92a573938291cd66128137f066187fc1d6f097
MustyX23 <mustyx900@gmail.com>
2023-02-26 23:09:03 +0200

    03. Extract File

---
+++ Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string file = input.Substring(input.LastIndexOf('\\') + 1);
+
+            int dotIndex = file.LastIndexOf('.');
+            string fileName = file.Substring(0, dotIndex);
+            string fileExtencion = file.Substring(dotIndex + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtencion}");
+        }
+    }
+}
