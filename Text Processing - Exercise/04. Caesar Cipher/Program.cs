3c854ed8802d2d91e939faf5dca19db8f6a57000
MustyX23 <mustyx900@gmail.com>
2023-02-26 23:09:14 +0200

    04. Caesar Cipher

---
+++ Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder encryptedMessage = new StringBuilder();
+
+            foreach (char symbol in input)
+            {
+                char nextSymobl = (char)(symbol + 3);
+                encryptedMessage.Append(nextSymobl);
+            }
+
+            Console.WriteLine(encryptedMessage);
+        }
+    }
+}
