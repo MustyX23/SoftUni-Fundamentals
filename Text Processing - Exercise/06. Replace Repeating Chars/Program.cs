24c771aa819c71af58b6cd5a88a5a5bab26863fd
MustyX23 <mustyx900@gmail.com>
2023-02-27 21:58:44 +0200

    06. Replace Repeating Chars

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int index = 1;
+
+            while (index != input.Length)
+            {
+                if (input[index - 1] == input[index])
+                {
+                    input = input.Remove(index, 1);
+                    index--;
+                }
+                index++;
+            }
+
+            Console.WriteLine(input);
+
+        }
+    }
+}
