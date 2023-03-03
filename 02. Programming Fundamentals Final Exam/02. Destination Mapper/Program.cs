e318cedabb326c860b838baf7ede587fb6b95d37
MustyX23 <mustyx900@gmail.com>
2023-03-03 11:52:36 +0200

    02. Destination Mapper

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<string> places = new List<string>();
+            
+            string pattern = @"(\=|\/)([A-Z][A-Za-z]{2,})\1";
+
+            int travelPoints = 0;
+
+            MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+            foreach (Match rawPlace in matchCollection)
+            {
+                places.Add(rawPlace.ToString().Trim('=', '/'));
+            }
+
+            foreach (string place in places)
+            {
+                travelPoints += place.Length;
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", places)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
