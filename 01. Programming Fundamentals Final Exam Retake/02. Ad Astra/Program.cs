fa42099617890564919f29378e46c9a3ed7bc808
MustyX23 <mustyx900@gmail.com>
2023-03-20 19:59:17 +0200

    02. Ad Astra

--- 01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
+++ 01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _02._Ad_Astra
 {
@@ -6,7 +7,34 @@ namespace _02._Ad_Astra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+
+            string pattern = @"(\||\#)(?<food>[A-Za-z\s]+)\1(?<date>(\d{2}\/)(\d{2}\/)(\d{2}))\1(?<calories>\d+)\1";
+
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            int neededCalories = 2000;
+            int totalCalories = 0;
+            int days = 0;
+
+            foreach (Match calorie in matches)
+            {                
+                int calories = int.Parse(calorie.Groups["calories"].Value);
+                totalCalories += calories;
+            }
+
+            days = totalCalories / neededCalories;
+
+            Console.WriteLine($"You have food to last you for: {days} days!");
+
+            foreach (Match item in matches)
+            {
+                string product = item.Groups["food"].Value;
+                string date = item.Groups["date"].Value;
+                int nutrition = int.Parse(item.Groups["calories"].Value);
+
+                Console.WriteLine($"Item: {product}, Best before: {date}, Nutrition: {nutrition}");
+            }
         }
     }
 }
